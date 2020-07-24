185205906c66032a44770faae9b3cb0ee2eb2c98
Albert <github@albert.sh>
2020-07-24 03:19:23 +0200

    Fix generation code

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 rm -rf cryptocurrency-icons
-git clone git@github.com:spothq/cryptocurrency-icons.git
+git clone https://github.com/spothq/cryptocurrency-icons.git
 npm run build
 node build/build.js
 npm run build
