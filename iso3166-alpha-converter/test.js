3e30da86188b1ddacb1aaf1645ce6593a6092636
corollari <corollari@users.noreply.github.com>
2020-08-09 17:30:05 +0000

    deploy: cf619869279e1c59435082567c9113c6a3a8c00d

--- iso3166-alpha-converter/test.js
+++ iso3166-alpha-converter/test.js
@@ -4,11 +4,11 @@ const assert = require('assert')
 
 assert.strictEqual(alpha2to3["us"], "USA")
 assert.strictEqual(alpha3to2["USA"], "us")
-Object.keys(alpha2to3).forEach(alpha2=>{
-  assert.match(alpha2, /^[a-z]{2}$/)
-  assert.match(alpha2to3[alpha2], /[A-Z]{3}/)
+Object.keys(alpha2to3).forEach(function(alpha2){
+  assert.ok(/^[a-z]{2}$/.test(alpha2))
+  assert.ok(/[A-Z]{3}/.test(alpha2to3[alpha2]))
 })
-Object.keys(alpha3to2).forEach(alpha3=>{
-  assert.match(alpha3, /^[A-Z]{3}$/)
-  assert.match(alpha3to2[alpha3], /[a-z]{2}/)
+Object.keys(alpha3to2).forEach(function(alpha3){
+  assert.ok(/^[A-Z]{3}$/.test(alpha3))
+  assert.ok(/[a-z]{2}/.test(alpha3to2[alpha3]))
 })
